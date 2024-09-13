1f25e3759ed59bc1001c7fc79d49a8dd4b876c01
caspermortensen <cmor21@student.aau.dk>
2024-09-13 13:27:43 +0200

    setting up the template app with react, TypeScript and tailwind

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl px-5 py-5 m-10 bg-slate-300  font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
+  );
+}
+
+export default App;
